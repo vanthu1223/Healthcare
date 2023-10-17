37f2deaa196628241155fa191ffcd185969d8f91
Bon <bon.a25@student.passerellesnumeriques.org>
2023-10-17 16:40:50 +0700

    fixbug

--- asserts/javaScript/login.js
+++ asserts/javaScript/login.js
@@ -1,1 +1,3 @@
-var a=5;
+fetch("https://example.com", {
+  credentials: "include",
+});
--- asserts/javaScript/service.js
+++ asserts/javaScript/service.js
@@ -1,25 +1,34 @@
-let slideIndex = 1;
-showSlides(slideIndex);
-function plusSlides(n) {
-     showSlides(slideIndex += n);
-}
+let productAPI = 'https://6520d354906e276284c4b3a6.mockapi.io/api/v1/products';
 
-function currentSlide(n) {
-    showSlides(slideIndex = n);
+function loadData() {
+  $.ajax({
+    type: 'GET',
+    contentType: 'application/json',
+    url: productAPI,
+    success: function(responseData) {
+      $("#dataProduct").html(responseData.map(function(item) {
+        return `
+          <tr>
+            <td>${item.id}</td>
+            <td>${item.name}</td>
+            <td>${item.price}</td>
+            <td><img src="${item.Avarta}" alt="Product Image width="100px" height = "100px" " /></td>
+            <td>${item.Avarta}</td>
+            <td>
+              <button class="btn btn-primary" onclick="update(${item.id})">update</button>
+              <button class="btn btn-primary" onclick="deleteProduct(${item.id})">delete</button>
+            </td>
+          </tr>
+        `;
+      }));
+    },
+    error: function(e) {
+      console.log(e);
+    }
+  });
 }
+// Gọi hàm loadData để tải dữ liệu khi trang được tải
+loadData();
 
-function showSlides(n) {
-    let i;
-    let slides = document.getElementsByClassName("mySlides");
-    let dots = document.getElementsByClassName("dot");
-    if (n > slides.length) {slideIndex = 1}    
-        if (n < 1) {slideIndex = slides.length}
-            for (i = 0; i < slides.length; i++) {
-                 slides[i].style.display = "none";  
-            }
-            for (i = 0; i < dots.length; i++) {
-                dots[i].className = dots[i].className.replace(" active", "");
-            }
-            slides[slideIndex-1].style.display = "block";  
-            dots[slideIndex-1].className += " active";
-}
+
+
