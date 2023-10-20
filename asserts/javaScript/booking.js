95b73f21896b06d5aafda8d660ea6a03eb1d75e2
Van Thao <levanthao@gmail.com>
2023-10-20 13:42:58 +0700

    combo-js

--- asserts/javaScript/booking.js
+++ asserts/javaScript/booking.js
@@ -24,3 +24,57 @@ $('.datepicker').datepicker({
 $('.datepicker').on("change", (e) => {
     console.log(e.target.value)
 })
+
+// // hiển thị các nút giờ.
+              // Lấy phần tử "time-picker" từ DOM
+              const timePicker = document.getElementById("time-picker");
+
+              // Mảng chứa các giờ bạn muốn hiển thị
+              const hours = [
+                "7:00", "8:00", "9:00", "10:00", "11:00",
+                "12:00", "13:00", "14:00", "15:00", "16:00",
+                "17:00", "18:00", "19:00", "20:00", "21:00"
+              ];
+
+              // Biến đếm để theo dõi số lượng nút đã thêm vào
+              let buttonCount = 0;
+
+              // Tạo hàng đầu tiên
+              let currentRow = document.createElement("div");
+              currentRow.className = "row time";
+
+              // Duyệt qua mảng giờ và tạo các nút radio và nhãn
+              hours.forEach(hour => {
+                const column = document.createElement("div");
+                column.className = "col-md-2 mb-2";
+
+                const radioInput = document.createElement("input");
+                radioInput.type = "radio";
+                radioInput.className = "time-picker";
+                radioInput.name = "aq";
+                radioInput.value = hour;
+                radioInput.id = "a" + hour.replace(":", "");
+
+                const label = document.createElement("label");
+                label.setAttribute("for", "a" + hour.replace(":", ""));
+                label.textContent = hour;
+                label.className = "time-label";
+
+                currentRow.appendChild(column);
+                column.appendChild(radioInput);
+                column.appendChild(label);
+
+                buttonCount++;
+
+                // Nếu đã thêm 5 nút, tạo một hàng mới
+                if (buttonCount % 5 === 0) {
+                  timePicker.appendChild(currentRow);
+                  currentRow = document.createElement("div");
+                  currentRow.className = "row time";
+                }
+              });
+
+              // Kiểm tra xem có cần thêm hàng cuối cùng hay không
+              if (buttonCount % 5 !== 0) {
+                timePicker.appendChild(currentRow);
+              }
